f8f58bc5b64e9c5ecae3e01ca0d8abe189e7b8c1
ItsZeroFour <ItsZeroFourX@gmail.com>
2023-06-03 21:23:05 +0300

    change broswer router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./container/Home/Home";
 import NotFound from "./components/NotFound/NotFound";
 import Registration from "./container/Registration/Registration";
@@ -22,37 +22,35 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <HashRouter>
-          <Routes>
-            <Route path="/accounting" element={<Home />} />
-            <Route path="/accounting/payments" element={<Payments />} />
-            <Route path="/accounting/registration" element={<Registration />} />
-            <Route path="/accounting/signin" element={<SignIn />} />
-            <Route path="/accounting/account" element={<Account />} />
-            <Route path="/accounting/settings" element={<Settings />} />
-            <Route
-              path="/acconting/forgot-password"
-              element={<ForgotPassword />}
-            />
-            <Route
-              path="/accounting/registration/code"
-              element={<SecureCode />}
-            />
-            <Route
-              path="/accounting/forgot-password/code"
-              element={<ForgotPasswordSecureCode />}
-            />
-            <Route
-              path="/accounting/reset-password/new-password"
-              element={<NewPassword />}
-            />
-            <Route
-              path="/accounting/registration/details"
-              element={<Details />}
-            />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </HashRouter>
+        <Routes>
+          <Route path="/accounting/" element={<Home />} />
+          <Route path="/accounting/payments" element={<Payments />} />
+          <Route path="/accounting/registration" element={<Registration />} />
+          <Route path="/accounting/signin" element={<SignIn />} />
+          <Route path="/accounting/account" element={<Account />} />
+          <Route path="/accounting/settings" element={<Settings />} />
+          <Route
+            path="/acconting/forgot-password"
+            element={<ForgotPassword />}
+          />
+          <Route
+            path="/accounting/registration/code"
+            element={<SecureCode />}
+          />
+          <Route
+            path="/accounting/forgot-password/code"
+            element={<ForgotPasswordSecureCode />}
+          />
+          <Route
+            path="/accounting/reset-password/new-password"
+            element={<NewPassword />}
+          />
+          <Route
+            path="/accounting/registration/details"
+            element={<Details />}
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
     </div>
   );
